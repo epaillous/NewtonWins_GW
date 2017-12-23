b1295e6dcebaec816f5162b94350a20f6a903e70
Emilie Paillous <emilie.paillous@gmail.com>
2017-12-23 14:00:12 -0300

    add point in json menu item

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -4,7 +4,7 @@ class MenuController < ApplicationController
     @menu_items = []
     continents = Continent.all
     continents.map do |continent|
-      @menu_items << {name: continent.name}
+      @menu_items << {name: continent.name, point: continent.point}
     end
     render json: {menu: @menu_items}
   end
